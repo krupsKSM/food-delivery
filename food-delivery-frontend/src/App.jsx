987e12aaf11d48449be68cf8f8362452c017b4cd
krupsKSM <krupasindhu.dev@gmail.com>
2025-07-21 07:41:07 +0530

    Add landing page structure and components

--- food-delivery-frontend/src/App.jsx
+++ food-delivery-frontend/src/App.jsx
@@ -1,8 +1,22 @@
+import Navbar from "./components/navbar/Navbar"
+import { Routes ,Route } from "react-router-dom"
+import Home from "./pages/home/Home"
+import Cart from "./pages/cart/Cart"
+import PlaceOrder from "./pages/placeOrder/PlaceOrder"
+
 function App(){
 
   return(
     <>
-      <h2>Food delivery</h2>
+    <div className="app">
+      {/* <h2>Food delivery</h2> */}
+      <Navbar/>
+      <Routes>
+        <Route path="/" element= {<Home/>}/>
+        <Route path = "/cart" element = {<Cart/>} />
+        <Route path = "/placeOrder" element = {<PlaceOrder/>} />
+      </Routes>
+    </div>
     </>
   )
 }
---
+++ food-delivery-frontend/src/pages/cart/Cart.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import "./cart.css"
+const Cart = () => {
+  return (
+    <div>
+      <h2>cartpage</h2>
+    </div>
+  )
+}
+
+export default Cart
